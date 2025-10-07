ff6ebcb355be8cf8f18a66625ad0be312c1b5ff5
Larissa Welti <weltil@msoe.edu>
2025-10-07 15:29:47 -0500

    Added .gitignore and added main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+/* FileName: main.c
+ * Author: Larissa Welti
+ * Date: 10.7.2025
+ * Description: Does basic vector math like addition, subtraction, and scalar multiplication
+ * can store up to 10 vectors to do math with
+*/
+
+#include <stdio.h>
+#include "vector_math.h"
+#include"vector.h"
